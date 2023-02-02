6110c6346dbb12c984b16c8c35fac5c59acb6272
Ali El-Sayed <alielsayedg@gmail.com>
2023-02-02 21:43:20 +0200

    Refactor and support Saved State

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,38 +4,54 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import java.security.Key
 
 /**
  * This activity allows the user to roll a dice and view the result
  * on the screen.
  */
+private const val KEY_DICE = "dice_number"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var rollButton: Button
+    private var dice: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        rollButton = findViewById(com.example.diceroller.R.id.btnRoll)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton = findViewById(R.id.btnRoll)
+        rollButton.setOnClickListener {
+            dice = rollDice()
+            setDiceImage(dice)
+        }
 
-        // Do a dice roll when the app starts
-        rollDice()
+        if (savedInstanceState != null) {
+            dice = savedInstanceState.getInt(KEY_DICE, 0)
+            setDiceImage(dice)
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_DICE, dice)
+
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice() {
+    private fun rollDice(): Int {
         // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
-        val diceRoll = dice.roll()
+        return dice.roll()
+    }
 
+    private fun setDiceImage(dice: Int) {
         // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imgDice)
 
         // Determine which drawable resource ID to use based on the dice roll
-        val drawableResource = when (diceRoll) {
+        val drawableResource = when (dice) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -48,6 +64,6 @@ class MainActivity : AppCompatActivity() {
         diceImage.setImageResource(drawableResource)
 
         // Update the content description
-        diceImage.contentDescription = diceRoll.toString()
+        diceImage.contentDescription = dice.toString()
     }
 }
